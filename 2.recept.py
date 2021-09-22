9bcfd691777639c2269eb1c98c9c9b9bdc9060c0
Dnykeee <90310155+Dnykeee@users.noreply.github.com>
2021-09-22 10:37:06 +0200

    Add files via upload

---
+++ 2.recept.py
@@ -0,0 +1,8 @@
+def szendvics(cola_type):
+    print('Hamburger zsömle.')
+    print('Belerakunk egy rántotthúst.')
+    print('Nyomunk bele majonézet.')
+    print('Rakunk bele salátát.')
+    print('Szolgáld fel ' + cola_type + 'val/vel.')
+
+szendvics('coca')
---
+++ 2szam.py
@@ -0,0 +1,5 @@
+a=int(input('Add meg az egyik számot: '))
+b=int(input('Add meg a másik számot: '))
+osszeg=a+b
+
+print=('A kettő szám összege: ', osszeg)
