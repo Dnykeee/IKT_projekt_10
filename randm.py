f456c31c31fc93df14711b51842052ce751a7349
Dnykeee <90310155+Dnykeee@users.noreply.github.com>
2021-11-10 10:30:42 +0100

    Add files via upload

---
+++ randm.py
@@ -0,0 +1,11 @@
+import os
+import random
+import time
+def clear():
+        os.system('CLS')
+
+for ismetles in range (10):
+    x=random.randint(1,6)
+    print(x)
+    time.sleep(2)
+    clear()
